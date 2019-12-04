5bc88e001aa3b95b700b2fd8e9a278c071f887fa
黄荣君 <l1091462907@gmail.com>
2019-12-04 15:47:06 +0800

    简化静态文件判断

--- summer-mis/src/main/java/cn/cerc/mis/config/AppStaticFileDefault.java
+++ summer-mis/src/main/java/cn/cerc/mis/config/AppStaticFileDefault.java
@@ -2,19 +2,44 @@ package cn.cerc.mis.config;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class AppStaticFileDefault implements IAppStaticFile {
 
+    private static List<String> suffix = new ArrayList<>();
+
+    {
+        // 网页文件
+        suffix.add(".css");
+        suffix.add(".js");
+        suffix.add(".jsp");
+        suffix.add(".htm");
+        suffix.add(".html");
+
+        // 图片文件
+        suffix.add(".jpg");
+        suffix.add(".png");
+        suffix.add(".gif");
+        suffix.add(".icon");
+        suffix.add(".bmp");
+
+        // 音频文件
+        suffix.add(".mp3");
+
+        // 视频文件
+        suffix.add(".mp4");
+
+        // 安装文件
+        suffix.add(".apk");
+        suffix.add(".exe");
+        suffix.add(".manifest");
+    }
+
     @Override
     public boolean isStaticFile(String uri) {
-        if (uri.endsWith(".css") || uri.endsWith(".jpg") || uri.endsWith(".gif") || uri.endsWith(".png")
-                || uri.endsWith(".bmp") || uri.endsWith(".js") || uri.endsWith(".mp3") || uri.endsWith(".icon")
-                || uri.endsWith(".apk") || uri.endsWith(".exe") || uri.endsWith(".jsp") || uri.endsWith(".htm")
-                || uri.endsWith(".html") || uri.endsWith(".manifest")) {
-            return true;
-        } else {
-            return false;
-        }
+        return suffix.stream().anyMatch(uri::endsWith);
     }
 
 }
