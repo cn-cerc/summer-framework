0dca17d1f17e31f2515f86d6f1aceb34f274fa0d
黄荣君 <l1091462907@gmail.com>
2019-12-09 19:25:50 +0800

    生命值小于1不允许登录

--- summer-mis/src/main/java/cn/cerc/mis/services/AppSessionRestore.java
+++ summer-mis/src/main/java/cn/cerc/mis/services/AppSessionRestore.java
@@ -40,12 +40,12 @@ public class AppSessionRestore extends CustomService {
         String token = headIn.getString("token");
 
         SqlQuery cdsCurrent = new SqlQuery(this);
-        cdsCurrent.add("select CorpNo_,UserID_,LoginTime_,Account_ as UserCode_,Language_ ");
+        cdsCurrent.add("select CorpNo_,UserID_,Viability_,LoginTime_,Account_ as UserCode_,Language_ ");
         cdsCurrent.add("from %s", systemTable.getCurrentUser());
         cdsCurrent.add("where loginID_= '%s' ", token);
         cdsCurrent.open();
         if (cdsCurrent.eof()) {
-            log.warn(String.format("token %s 没有找到！", token));
+            log.warn("token {} 没有找到！", token);
             HandleDefault sess = (HandleDefault) this.getProperty(null);
             sess.setProperty(Application.token, null);
             return false;
