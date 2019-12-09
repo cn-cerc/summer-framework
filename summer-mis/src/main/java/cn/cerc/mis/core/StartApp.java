1b3c5d83b94a4019773417534db16caa19502e0d
黄荣君 <l1091462907@gmail.com>
2019-12-09 20:09:10 +0800

    退出路径统一改为 /public/ 目录

--- summer-mis/src/main/java/cn/cerc/mis/core/StartApp.java
+++ summer-mis/src/main/java/cn/cerc/mis/core/StartApp.java
@@ -2,6 +2,7 @@ package cn.cerc.mis.core;
 
 import cn.cerc.core.IHandle;
 import cn.cerc.db.core.IAppConfig;
+import cn.cerc.mis.config.AppProperty;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.Filter;
@@ -36,7 +37,7 @@ public class StartApp implements Filter {
                         req.getParameter(ClientDevice.APP_DEVICE_TYPE));
 
             IAppConfig conf = Application.getAppConfig();
-            resp.sendRedirect(String.format("/public/%s", conf.getFormWelcome()));
+            resp.sendRedirect(String.format("%s%s", AppProperty.App_Path, conf.getFormWelcome()));
             return;
         } else if (uri.equals("/MobileConfig") || uri.equals("/mobileConfig")) {
             if (req.getParameter(ClientDevice.APP_CLIENT_ID) != null)
