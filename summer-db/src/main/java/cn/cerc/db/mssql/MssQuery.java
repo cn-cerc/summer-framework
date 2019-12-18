60f155b970263d0ee0394888f2b85dc5b436d7d6
黄荣君 <l1091462907@gmail.com>
2019-12-18 21:40:00 +0800

    增加mssql数据库连接范例

--- summer-db/src/main/java/cn/cerc/db/mssql/MssQuery.java
+++ summer-db/src/main/java/cn/cerc/db/mssql/MssQuery.java
@@ -5,8 +5,11 @@ import cn.cerc.core.IDataOperator;
 import cn.cerc.core.IHandle;
 
 public class MssQuery extends DataQuery {
+
     private static final long serialVersionUID = 889285738942368226L;
 
+    private MssqlConnection session;
+    
     public MssQuery(IHandle handle) {
         super(handle);
     }
