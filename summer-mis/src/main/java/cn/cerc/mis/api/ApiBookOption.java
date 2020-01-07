e7b9563d9e02a8ff54c35dcb479fb1062c93c38e
颜家智 <18476661381@163.com>
2020-01-07 16:23:43 +0800

    Update ApiBookOption.java

--- summer-mis/src/main/java/cn/cerc/mis/api/ApiBookOption.java
+++ summer-mis/src/main/java/cn/cerc/mis/api/ApiBookOption.java
@@ -2,7 +2,6 @@ package cn.cerc.mis.api;
 
 import cn.cerc.core.Record;
 import cn.cerc.core.Utils;
-import cn.cerc.db.mysql.BuildQuery;
 import cn.cerc.db.mysql.SqlQuery;
 import cn.cerc.mis.core.CustomService;
 import cn.cerc.mis.core.DataValidateException;
@@ -21,11 +20,11 @@ public class ApiBookOption extends CustomService {
         DataValidateException.stopRun("参数代码不允许为空", !headIn.hasValue("Code_"));
         String code = Utils.safeString(headIn.getString("Code_"));
 
-        BuildQuery f = new BuildQuery(this);
-        f.add("select Value_ from %s ", systemTable.getBookOptions());
-        f.byField("CorpNo_", corpNo);
-        f.byField("Code_", code);
-        SqlQuery cdsTmp = f.open();
+        SqlQuery cdsTmp = new SqlQuery(this);
+        cdsTmp.add("select Value_ from %s", systemTable.getBookOptions());
+        cdsTmp.add("where CorpNo_='%s'", corpNo);
+        cdsTmp.add("and Code_='%s'", code);
+        cdsTmp.open();
 
         getDataOut().appendDataSet(cdsTmp);
         return true;
