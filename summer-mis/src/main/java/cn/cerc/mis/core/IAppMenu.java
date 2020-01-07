c3182c4fde4b75d563cddc8260b33446e1256e73
黄荣君 <l1091462907@gmail.com>
2020-01-07 13:57:34 +0800

    Update IAppMenu.java

--- summer-mis/src/main/java/cn/cerc/mis/core/IAppMenu.java
+++ summer-mis/src/main/java/cn/cerc/mis/core/IAppMenu.java
@@ -11,7 +11,7 @@ public interface IAppMenu {
 
     // 返回系统所有的Module
     public List<MenuItem> getModule(IHandle handle);
-    
+
     // 返回指定父菜单下的所有子菜单
     public List<MenuItem> getList(IHandle handle, String parentId, boolean security);
 }
