a31ae5628b95b3d1e2d0f66fdcc220eb2f77b3c6
黄荣君 <l1091462907@gmail.com>
2020-04-14 15:40:21 +0800

    凌晨1点整重新初始化token

--- summer-mis/src/main/java/cn/cerc/mis/task/ProcessTimerTask.java
+++ summer-mis/src/main/java/cn/cerc/mis/task/ProcessTimerTask.java
@@ -12,6 +12,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.TimerTask;
 
@@ -22,6 +23,8 @@ public class ProcessTimerTask extends TimerTask implements ApplicationContextAwa
     // 晚上12点执行，也即0点开始执行
     private static final int C_SCHEDULE_HOUR = 0;
     private static boolean isRunning = false;
+    private static final String One_O_Clock = "01:00:00";
+
     private static String lock;
     private IHandle handle;
 
@@ -43,11 +46,7 @@ public class ProcessTimerTask extends TimerTask implements ApplicationContextAwa
                 }
             } else if (ServerConfig.enableTaskService()) {
                 try {
-                    // 初始化特殊用户的handle
-                    if (handle == null) {
-                        handle = new StubHandle();
-                    }
-                    runTask(handle);
+                    runTask();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -58,14 +57,16 @@ public class ProcessTimerTask extends TimerTask implements ApplicationContextAwa
         }
     }
 
-    private void runTask(IHandle handle) {
+    private void runTask() {
+        init();
+
         // 同一秒内，不允许执行2个及以上任务
         String str = TDateTime.Now().getTime();
         if (str.equals(lock)) {
             return;
         }
-
         lock = str;
+
         for (String beanId : context.getBeanNamesForType(AbstractTask.class)) {
             AbstractTask task = getTask(handle, beanId);
             if (task == null) {
@@ -98,6 +99,26 @@ public class ProcessTimerTask extends TimerTask implements ApplicationContextAwa
         }
     }
 
+    /**
+     * 初始化特殊用户的 handle
+     */
+    private void init() {
+        if (handle == null) {
+            handle = new StubHandle();
+        }
+
+        // 凌晨1点整重新初始化token
+        LocalTime now = LocalTime.now().withNano(0);
+        if (One_O_Clock.equals(now.toString())) {
+            if (handle != null) {
+                handle.close();
+                handle = null;
+            }
+            log.warn("{} 队列重新初始化句柄", TDateTime.Now());
+            handle = new StubHandle();
+        }
+    }
+
     public static AbstractTask getTask(IHandle handle, String beanId) {
         AbstractTask task = Application.getBean(beanId, AbstractTask.class);
         if (task != null) {
@@ -115,4 +136,5 @@ public class ProcessTimerTask extends TimerTask implements ApplicationContextAwa
     public void setApplicationContext(ApplicationContext context) throws BeansException {
         this.context = context;
     }
+
 }
