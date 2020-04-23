9c0487c5c7ba78f06bc46a4815d832a748811e61
黄荣君 <l1091462907@gmail.com>
2020-04-23 09:56:24 +0800

    Update IAppMenu.java

--- summer-mis/src/main/java/cn/cerc/mis/core/IAppMenu.java
+++ summer-mis/src/main/java/cn/cerc/mis/core/IAppMenu.java
@@ -7,6 +7,10 @@ import java.util.List;
 public interface IAppMenu {
 
     // 返回系统菜单定义
+    @Deprecated
+    /**
+     * 此项在最新版的地藤版本中不再使用
+     */
     MenuItem getItem(String menuId);
 
     // 返回系统所有的Module
