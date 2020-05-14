14023ec9981100e57736ba38e5a4871ae1c78fd3
黄荣君 <l1091462907@gmail.com>
2020-05-14 18:07:26 +0800

    Update FileUploadPage.java

--- summer-mis/src/main/java/cn/cerc/mis/page/upload/FileUploadPage.java
+++ summer-mis/src/main/java/cn/cerc/mis/page/upload/FileUploadPage.java
@@ -45,22 +45,22 @@ public class FileUploadPage extends FileUploadBasePage {
     @Override
     public IPage exec() {
         UIPageSearch jspPage = new UIPageSearch(this);
-        UIHeader top = jspPage.getHeader();
+        UIHeader header = jspPage.getHeader();
         Map<String, String> menus = getMenuPath();
         if (!menus.isEmpty()) {
             for (String key : menus.keySet()) {
-                top.addLeftMenu(key, R.asString(this, menus.get(key)));
+                header.addLeftMenu(key, R.asString(this, menus.get(key)));
             }
         }
 
         setName(R.asString(this, getPageTitle()));
-        top.setPageTitle(R.asString(this, getPageTitle()));
+        header.setPageTitle(R.asString(this, getPageTitle()));
 
         jspPage.addScriptFile("../imgZoom/imgAlert.js");
         jspPage.addCssFile("../imgZoom/bigImg.css");
 
-        UIToolbar right = jspPage.getToolBar();
-        UISheetHelp section1 = new UISheetHelp(right);
+        UIToolbar toolbar = jspPage.getToolBar();
+        UISheetHelp section1 = new UISheetHelp(toolbar);
         section1.setCaption(R.asString(this, "操作提示"));
         section1.addLine(R.asString(this, "所支持上传的文件类型："), getSuportTypes());
         section1.addLine(getSuportTypes());
