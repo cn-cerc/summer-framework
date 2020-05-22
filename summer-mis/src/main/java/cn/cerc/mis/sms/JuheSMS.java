bec47f0c49477d59a505eefa0c5c8a9c81a6cf5f
黄荣君 <l1091462907@gmail.com>
2020-05-22 15:28:04 +0800

    Update JuheSMS.java

--- summer-mis/src/main/java/cn/cerc/mis/sms/JuheSMS.java
+++ summer-mis/src/main/java/cn/cerc/mis/sms/JuheSMS.java
@@ -1,8 +1,8 @@
-package cn.cerc.mis.sms;
+package cn.cerc.sms;
 
-import cn.cerc.db.core.ServerConfig;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import net.sf.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -16,83 +16,93 @@ import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 短信API服务调用示例代码 － 聚合数据 在线接口文档：http://www.juhe.cn/docs/54
- **/
-
 @Slf4j
 public class JuheSMS {
 
     public static final String DEF_CHATSET = "UTF-8";
     public static final int DEF_CONN_TIMEOUT = 5000;
     public static final int DEF_READ_TIMEOUT = 5000;
-    public static final String userAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
-    // 配置您申请的KEY
-    public static String APPKEY = "";
+    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
+    /**
+     * 请求地址
+     **/
+    private static final String Juhe_Url = "http://v.juhe.cn/sms/send";
+    private static Map<String, String> items = new HashMap<>();
 
     static {
-        ServerConfig config = ServerConfig.getInstance();
-        APPKEY = config.getProperty("juhe.sms");// juhe sms
+        // 系统级错误码参照
+        items.put("10001", "错误的请求KEY");
+        items.put("10002", "该KEY无请求权限");
+        items.put("10003", "KEY过期");
+        items.put("10004", "错误的OPENID");
+        items.put("10005", "应用未审核超时，请提交认证");
+        items.put("10007", "未知的请求源");
+        items.put("10008", "被禁止的IP");
+        items.put("10009", "被禁止的KEY");
+        items.put("10011", "当前IP请求超过限制");
+        items.put("10012", "请求超过次数限制");
+        items.put("10013", "测试KEY超过请求限制");
+        items.put("10014", "系统内部异常");
+        items.put("10020", "接口维护");
+        items.put("10021", "接口停用");
+
+        // 服务级错误码参照
+        items.put("205401", "错误的手机号码");
+        items.put("205402", "错误的短信模板ID");
+        items.put("205403", "网络错误,请重试");
+        items.put("205404", "发送失败，具体原因请参考返回reason");
+        items.put("205405", "号码异常/同一号码发送次数过于频繁");
+        items.put("205406", "不被支持的模板");
     }
 
-    //
-    private String mobile;
     private String message;
+    private String apiKey;
 
-    public JuheSMS(String mobile) {
-        this.mobile = mobile;
+    public JuheSMS(String appKey) {
+        this.apiKey = appKey;
     }
 
-    // 1.屏蔽词检查测
-    protected void getRequest1() {
-        String result = null;
-        String url = "http://v.juhe.cn/sms/black";// 请求接口地址
-        Map<String, Object> params = new HashMap<>();// 请求参数
-        params.put("word", "");// 需要检测的短信内容，需要UTF8 URLENCODE
-        params.put("key", APPKEY);// 应用APPKEY(应用详细页查询)
-
+    /**
+     * @param mobile     接收者手机号
+     * @param templateId 短信模板编号
+     * @param text       内容格式化
+     * @return 发送结果
+     */
+    public boolean send(String mobile, String templateId, String text) {
         try {
-            result = post(url, params, "GET");
-            JSONObject object = JSONObject.fromObject(result);
-            if (object.getInt("error_code") == 0) {
-                log.info("" + object.get("result"));
-            } else {
-                log.info("" + object.get("error_code") + ":" + object.get("reason"));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+            Map<String, Object> params = new HashMap<>();
+            // 接收短信的手机号码
+            params.put("mobile", mobile);
+            // 短信模板ID，请参考个人中心短信模板设置
+            params.put("tpl_id", templateId);
+            // 变量名和变量值对
+            params.put("tpl_value", URLEncoder.encode(text, "UTF-8"));
+            // 应用APPKEY(应用详细页查询)
+            params.put("key", this.apiKey);
+            // 返回数据的格式,xml或json，默认json
+            params.put("dtype", "");
 
-    // 2.发送短信
-    public boolean sendByTemplateId(String templateId, String templateValues) {
-        String result = null;
-        String url = "http://v.juhe.cn/sms/send";// 请求接口地址
-        Map<String, Object> params = new HashMap<>();// 请求参数
-        params.put("mobile", mobile);// 接收短信的手机号码
-        params.put("tpl_id", templateId);// 短信模板ID，请参考个人中心短信模板设置
-        params.put("tpl_value", templateValues);// 变量名和变量值对。如果你的变量名或者变量值中带有#&=中的任意一个特殊符号，请先分别进行urlencode编码后再传递，<a
-        // href="http://www.juhe.cn/news/index/id/50"
-        // target="_blank">详细说明></a>
-        params.put("key", APPKEY);// 应用APPKEY(应用详细页查询)
-        params.put("dtype", "");// 返回数据的格式,xml或json，默认json
+            String response = post(Juhe_Url, params, "GET");
 
-        try {
-            result = post(url, params, "GET");
-            JSONObject object = JSONObject.fromObject(result);
-            if (object.getInt("error_code") == 0) {
-                log.info("" + object.get("result"));
-                this.message = object.getString("result");
-                log.info(String.format("send: %s, templateId: %s, result: %s", mobile, templateId, message));
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode json = mapper.readTree(response);
+
+            if (json.get("error_code").asInt() == 0) {
+                String tailNumber = mobile.substring(mobile.length() - 4);
+                this.setMessage(String.format(RS.验证码发送成功, tailNumber));
+
+                String result = json.get("result").asText();
+                log.info("send: {}, templateId: {}, result: {}", mobile, templateId, result);
                 return true;
             } else {
-                this.message = object.get("error_code") + ":" + object.get("reason");
-                log.warn(String.format("send: %s, templateId: %s, result: %s", mobile, templateId, message));
+                String error_code = json.get("error_code").asText();
+                this.setMessage("%s: %s", error_code, items.get(error_code));
+                log.error("send: {}, templateId: {}, result: {}", mobile, templateId, getMessage());
                 return false;
             }
         } catch (Exception e) {
-            this.message = e.getMessage();
-            log.error(String.format("send: %s, templateId: %s, result: %s", mobile, templateId, message));
+            this.setMessage(e.getMessage());
+            log.error("send: {}, templateId: {}, result: {}", mobile, templateId, getMessage());
             return false;
         }
     }
@@ -103,24 +113,24 @@ public class JuheSMS {
         String rs = null;
         try {
             StringBuffer sb = new StringBuffer();
-            if (method == null || method.equals("GET")) {
+            if (method == null || "GET".equals(method)) {
                 strUrl = strUrl + "?" + urlEncode(params);
             }
             URL url = new URL(strUrl);
             conn = (HttpURLConnection) url.openConnection();
-            if (method == null || method.equals("GET")) {
+            if (method == null || "GET".equals(method)) {
                 conn.setRequestMethod("GET");
             } else {
                 conn.setRequestMethod("POST");
                 conn.setDoOutput(true);
             }
-            conn.setRequestProperty("User-agent", userAgent);
+            conn.setRequestProperty("User-agent", USER_AGENT);
             conn.setUseCaches(false);
             conn.setConnectTimeout(DEF_CONN_TIMEOUT);
             conn.setReadTimeout(DEF_READ_TIMEOUT);
             conn.setInstanceFollowRedirects(false);
             conn.connect();
-            if (params != null && method.equals("POST")) {
+            if (params != null && "POST".equals(method)) {
                 try {
                     DataOutputStream out = new DataOutputStream(conn.getOutputStream());
                     out.writeBytes(urlEncode(params));
@@ -149,7 +159,7 @@ public class JuheSMS {
     }
 
     // 将map型转为请求参数型
-    protected String urlEncode(Map<String, Object> data) {
+    private String urlEncode(Map<String, Object> data) {
         StringBuilder sb = new StringBuilder();
         try {
             for (String key : data.keySet()) {
@@ -166,4 +176,13 @@ public class JuheSMS {
     public String getMessage() {
         return message;
     }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setMessage(String format, Object... args) {
+        this.message = String.format(format, args);
+    }
+
 }
