6a841069dd4d7e9991d990a1a78feeb3cf5c98fc
黄荣君 <l1091462907@gmail.com>
2020-06-10 20:38:13 +0800

    Update SvrUserLogin.java

--- summer-mis/src/main/java/cn/cerc/mis/services/SvrUserLogin.java
+++ summer-mis/src/main/java/cn/cerc/mis/services/SvrUserLogin.java
@@ -551,6 +551,7 @@ public class SvrUserLogin extends CustomService {
     public void updateCurrentUser(String computer, String screen, String language) {
 //        getConnection().execute(String.format("Update %s Set Viability_=0 Where Viability_>0 and LogoutTime_<'%s'",
 //                systemTable.getCurrentUser(), TDateTime.Now().incHour(-1)));
+        // FIXME: 2020/6/10 一次只能有一个token存活，此项有bug，没有清理掉redis缓存的信息，造成HandleDefault的缓存还可以使用
         String SQLCmd = String.format(
                 "update %s set Viability_=-1,LogoutTime_='%s' where Account_='%s' and Viability_>-1",
                 systemTable.getCurrentUser(), TDateTime.Now(), getUserCode());
