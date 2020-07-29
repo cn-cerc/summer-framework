7701bbdf4e5a7db03e250442f0c004b6a4be9bd2
黄荣君 <l1091462907@gmail.com>
2020-07-29 14:46:19 +0800

    导航栏的和系统背景图改为从cdn读取

--- summer-mis/src/main/java/cn/cerc/ui/UIConfig.java
+++ summer-mis/src/main/java/cn/cerc/ui/UIConfig.java
@@ -1,6 +1,19 @@
 package cn.cerc.ui;
 
+import cn.cerc.mis.cdn.CDN;
+
 public class UIConfig {
-    public static final String EASY_PIC_5_PC = "images/easypic5-max-pc.png";
-    public static final String GUIDE = "images/guide.png";
+
+    // 开始图标
+    public static final String home_index = CDN.get("images/Home.png");
+
+    // 默认logo
+    public static final String app_logo = CDN.get("images/logo_dt.png");
+
+    // 系统背景图
+    public static final String EASY_PIC_5_PC = CDN.get("images/easypic5-max-pc.png");
+
+    // 操作说明
+    public static final String GUIDE = CDN.get("images/guide.png");
+
 }
