0c80d61f802478d9844387d711e9ef40140d4ce2
黄荣君 <l1091462907@gmail.com>
2020-12-14 19:54:40 +0800

    发送消息增加时间戳

--- summer-db/src/test/java/cn/cerc/db/jiguang/JiguangPushTest.java
+++ summer-db/src/test/java/cn/cerc/db/jiguang/JiguangPushTest.java
@@ -1,9 +1,12 @@
 package cn.cerc.db.jiguang;
 
+import cn.cerc.core.TDateTime;
 import cn.cerc.db.core.StubHandleText;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
+@Slf4j
 public class JiguangPushTest {
     private static final String sound = "trade_mall.wav";
     private StubHandleText handle;
@@ -22,14 +25,16 @@ public class JiguangPushTest {
         push.setTitle("消息推送测试");
 
         // 通知栏消息内容
-        push.setMessage("这是系统向您发送的测试消息，如有打扰，请您忽略，谢谢！");
+        String message = TDateTime.now().toString() + "这是系统向您发送的测试消息，如有打扰，请您忽略，谢谢！";
+        log.info(message);
+        push.setMessage(message);
 
         // 附加的消息id
         push.setMsgId("3707");
 
         // 发送给指定的设备Id
         push.send(ClientType.Android, "n_e201ff3670a4174b");// itjun-xiaomi-mix2
-        push.send(ClientType.Android, "n_862391030069496");// weibo-xiaomi-5c
+        push.send(ClientType.Android, "n_a86ddf4df465a83f");// weibo-xiaomi-5c
 //        push.send(ClientType.IOS, "i_59C532BB213C41D5BE3A117EB4D9FB67");// HuangRongjun-iPhone
 //        push.send(ClientType.IOS, "i_77598CF689994EE3B110D6B20F1368B7", sound);// HuangRongjun-iPhone
 
