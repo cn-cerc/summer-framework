426995ae26da4da61064faa29d0557d2a8cd54e7
IT君 <l1091462907@gmail.com>
2021-02-22 10:18:14 +0800

    add mail record

---
+++ summer-mvc/src/main/java/cn/cerc/mis/mail/AppMailQueue.java
@@ -0,0 +1,10 @@
+package cn.cerc.mis.mail;
+
+public class AppMailQueue {
+    // 每晚自动邮件
+    public static final String queueAutoMail = "automail";
+    // 业务手动邮件
+    public static final String queueSendMail = "sendmail";
+    // 测试队列
+    public static final String queueTest = "test";
+}
---
+++ summer-mvc/src/main/java/cn/cerc/mis/mail/MailRecord.java
@@ -0,0 +1,72 @@
+package cn.cerc.mis.mail;
+
+import cn.cerc.core.IHandle;
+import cn.cerc.db.queue.AliyunQueueConnection;
+import com.aliyun.mns.client.CloudQueue;
+import com.google.gson.Gson;
+
+import java.io.Serializable;
+
+public class MailRecord implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private IHandle handle;
+    private String to_addr;
+    private String to_name;
+    private String subject;
+    private String content;
+
+    public MailRecord(IHandle handle) {
+        this.handle = handle;
+    }
+
+    public boolean send() {
+        AliyunQueueConnection sess = (AliyunQueueConnection) handle.getProperty(AliyunQueueConnection.sessionId);
+        CloudQueue queue = sess.openQueue(AppMailQueue.queueSendMail);
+        return sess.append(queue, this.toString());
+    }
+
+    public MailRecord(String to, String subject, String content) {
+        this.to_addr = to;
+        this.subject = subject;
+        this.content = content;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTo_addr() {
+        return to_addr;
+    }
+
+    public void setTo_addr(String to_addr) {
+        this.to_addr = to_addr;
+    }
+
+    public String getTo_name() {
+        return to_name;
+    }
+
+    public void setTo_name(String to_name) {
+        this.to_name = to_name;
+    }
+
+    @Override
+    public String toString() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+}
