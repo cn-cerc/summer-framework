dfacf4d1ed77e12b42a03ed9d46e3b2205ab0dbc
张弓 <sz9214e@qq.com>
2021-03-09 09:40:26 +0800

    解决对execute与其它函数的共同支持

--- summer-mvc/src/main/java/cn/cerc/mis/core/StartFormDefault.java
+++ summer-mvc/src/main/java/cn/cerc/mis/core/StartFormDefault.java
@@ -31,17 +31,19 @@ public class StartFormDefault implements ApplicationContextAware {
 
     @RequestMapping("/{formId}")
     public ModelAndView execute(@PathVariable String formId) {
-        log.info("formId: {}", formId);
-
-        Application.setContext(context);
-        String viewId = Application.getFormView(request, response, formId, "execute");
-        return viewId != null ? new ModelAndView(viewId) : null;
-    }
-
-    @RequestMapping("/{formId}.{funcId}")
-    public ModelAndView execute(@PathVariable String formId, @PathVariable String funcId) {
-        log.info(String.format("formId: %s, funcId: %s", formId, funcId));
+        log.info("uri: {}", request.getRequestURI());
         
+        String funcId = "execute";
+        String[] args = request.getRequestURI().split("/");
+        for(int i =0; i< args.length; i++) {
+            if(args[i].startsWith(formId)) {
+                String[] tmp = args[i].split("\\.");
+                if(tmp.length > 1)
+                    funcId = tmp[1];
+                break;
+            }
+        }
+             
         Application.setContext(context);
         String viewId = Application.getFormView(request, response, formId, funcId);
         return viewId != null ? new ModelAndView(viewId) : null;
