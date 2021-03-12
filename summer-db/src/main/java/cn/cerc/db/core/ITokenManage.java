891fa8a2803966e3df6c5697b83c2513fb890ee7
张弓 <sz9214e@qq.com>
2021-03-12 15:06:26 +0800

    Update ITokenManage.java

--- summer-db/src/main/java/cn/cerc/db/core/ITokenManage.java
+++ summer-db/src/main/java/cn/cerc/db/core/ITokenManage.java
@@ -2,6 +2,7 @@ package cn.cerc.db.core;
 
 import cn.cerc.core.ISession;
 
+//此接口由原来的 IHandle 中分离出来
 public interface ITokenManage extends ISession{
     // 根据帐号密码等，生成新的 token，原命名为 init
     boolean createToken(String bookNo, String userCode, String password, String machineCode);
