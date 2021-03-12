6752c9144b021e11f9b5536256bd4f33aed5cf57
张弓 <sz9214e@qq.com>
2021-03-12 16:19:39 +0800

    增加 SupportSession

--- summer-db/src/main/java/cn/cerc/db/core/SupportHandle.java
+++ summer-db/src/main/java/cn/cerc/db/core/SupportHandle.java
@@ -1,5 +1,7 @@
 package cn.cerc.db.core;
 
+@Deprecated
+//FIXME 请改用 SupportSession
 public interface SupportHandle {
     void init(IHandle handle);
 }
---
+++ summer-db/src/main/java/cn/cerc/db/core/SupportSession.java
@@ -0,0 +1,8 @@
+package cn.cerc.db.core;
+
+import cn.cerc.core.ISession;
+
+public interface SupportSession {
+    
+    void setSession(ISession session);
+}
