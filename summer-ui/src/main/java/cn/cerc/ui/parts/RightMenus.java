3d7286006f7568a80bfc6e7d56a2f052e0a06c25
张弓 <sz9214e@qq.com>
2021-03-12 14:56:37 +0800

    Update RightMenus.java

--- summer-ui/src/main/java/cn/cerc/ui/parts/RightMenus.java
+++ summer-ui/src/main/java/cn/cerc/ui/parts/RightMenus.java
@@ -1,6 +1,6 @@
 package cn.cerc.ui.parts;
 
-import cn.cerc.mis.core.AbstractHandle;
+import cn.cerc.db.core.IHandle;
 import cn.cerc.ui.mvc.IMenuBar;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-public class RightMenus extends AbstractHandle {
+public class RightMenus extends IHandle {
     private List<IMenuBar> items = new ArrayList<>();
 
     public List<IMenuBar> getItems() {
