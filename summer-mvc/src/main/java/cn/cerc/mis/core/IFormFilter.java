51258882be81e0679bb28ae045ab14891488b6fc
张弓 <sz9214e@qq.com>
2021-03-13 11:52:53 +0800

    Update IFormFilter.java

--- summer-mvc/src/main/java/cn/cerc/mis/core/IFormFilter.java
+++ summer-mvc/src/main/java/cn/cerc/mis/core/IFormFilter.java
@@ -3,8 +3,6 @@ package cn.cerc.mis.core;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Deprecated
-//TODO IFormFilter 写法不容易配置，应改进
 public interface IFormFilter {
     boolean doFilter(HttpServletResponse resp, String formId, String funcCode) throws IOException;
 }
