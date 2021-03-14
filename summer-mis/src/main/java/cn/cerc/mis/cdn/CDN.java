d00bc07bbe87f27ace75d6e91b9413dfafc5e7d2
张弓 <sz9214e@qq.com>
2021-03-14 22:20:51 +0800

    Update CDN.java

--- summer-mis/src/main/java/cn/cerc/mis/cdn/CDN.java
+++ summer-mis/src/main/java/cn/cerc/mis/cdn/CDN.java
@@ -9,25 +9,31 @@ public class CDN {
     private static final ClassConfig config = new ClassConfig(CDN.class, SummerMIS.ID);
     // 启用内容网络分发
     public static final String OSS_CDN_ENABLE = "oss.cdn.enable";
-    //浏览器缓存版本号
+    // 浏览器缓存版本号
     public static final String BROWSER_CACHE_VERSION = "browser.cache.version";
 
     @Deprecated
     public static String getSite() {
         // 获取cdn的地址
-        String site = config.getString(OssConnection.oss_site, "");
+        String site = "";
         // 判断cdn是否启用
-        if (!Utils.isEmpty(site) && config.getBoolean(OSS_CDN_ENABLE, false))
-            site += "/resources/";
+        if (config.getBoolean(OSS_CDN_ENABLE, false)) {
+            site = config.getString(OssConnection.oss_site, "");
+            if (!Utils.isEmpty(site))
+                site += "/resources/";
+        }
         return site;
     }
 
     public static String get(String file) {
         // 获取cdn的地址
-        String site = config.getString(OssConnection.oss_site, "");
+        String site = "";
         // 判断cdn是否启用
-        if (!Utils.isEmpty(site) && config.getBoolean(OSS_CDN_ENABLE, false))
-            site += "/resources/";
+        if (config.getBoolean(OSS_CDN_ENABLE, false)) {
+            site = config.getString(OssConnection.oss_site, "");
+            if (!Utils.isEmpty(site))
+                site += "/resources/";
+        }
         return site + file + "?v=" + config.getString(BROWSER_CACHE_VERSION, "1.0.0.0");
     }
 }
