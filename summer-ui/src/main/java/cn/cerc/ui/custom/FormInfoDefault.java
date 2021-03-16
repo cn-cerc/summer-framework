fd2858b4b8cbaf2fcfbad58ec7725e33504e50f9
张弓 <sz9214e@qq.com>
2021-03-16 14:21:32 +0800

    把从数据库中取菜单名称的代码剥离出来

---
+++ summer-ui/src/main/java/cn/cerc/ui/custom/FormInfoDefault.java
@@ -0,0 +1,22 @@
+package cn.cerc.ui.custom;
+
+import cn.cerc.core.Utils;
+import cn.cerc.mis.core.IForm;
+import cn.cerc.mis.language.R;
+import cn.cerc.ui.code.IFormInfo;
+import cn.cerc.ui.menu.MenuList;
+
+public class FormInfoDefault implements IFormInfo {
+
+    @Override
+    public String getFormCaption(IForm form, String formId, String defaultValue) {
+        //FIXME 不得对函数使用R.asString，此处需要改进! ZhangGong 2021/3/16
+        if (Utils.isNotEmpty(form.getName())) {
+            return R.asString(form.getHandle(), form.getName());
+        } else {
+            return R.asString(form.getHandle(), MenuList.create(form.getHandle()).getName(formId));
+        }
+        
+    }
+
+}
