03d525ea8c74c7638a6d760b54672c1bfcaece4e
张弓 <sz9214e@qq.com>
2021-03-18 08:07:10 +0800

    从IPage中分离中IView接口

--- summer-mis/src/main/java/cn/cerc/mis/core/IPage.java
+++ summer-mis/src/main/java/cn/cerc/mis/core/IPage.java
@@ -1,16 +1,9 @@
 package cn.cerc.mis.core;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-public interface IPage {
-    IForm getForm();
-
-    void setForm(IForm form);
-
-    String execute() throws ServletException, IOException;
+public interface IPage extends IView {
 
     default HttpServletRequest getRequest() {
         IForm form = getForm();
