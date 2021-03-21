e213ef7468f20b067e4fa7180219911f96536b69
张弓 <sz9214e@qq.com>
2021-03-21 20:37:09 +0800

    从AbstractField中分离出相应的属性

--- summer-ui/src/main/java/cn/cerc/ui/fields/AbstractField.java
+++ summer-ui/src/main/java/cn/cerc/ui/fields/AbstractField.java
@@ -529,50 +529,6 @@ public abstract class AbstractField extends UIOriginComponent implements IField,
         }
     }
 
-    public class FieldTitle {
-        private String name;
-        private String type;
-        private String dateFormat;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public String getDateFormat() {
-            return dateFormat;
-        }
-
-        public void setDateFormat(String dateFormat) {
-            this.dateFormat = dateFormat;
-        }
-
-        @Override
-        public String toString() {
-            ObjectMapper mapper = new ObjectMapper();
-            ObjectNode json = mapper.createObjectNode();
-            json.put("name", this.name);
-            if (this.type != null) {
-                json.put("type", this.type);
-            }
-            if (this.dateFormat != null) {
-                json.put("dateFormat", this.dateFormat);
-            }
-            return json.toString().replace("\"", "'");
-        }
-    }
-
     public DataSource getDataSource() {
         return dataSource;
     }
