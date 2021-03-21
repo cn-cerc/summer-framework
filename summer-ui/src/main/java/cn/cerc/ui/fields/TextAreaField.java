36ff64a697dec12062e3ce6c713cea8c72abc6ac
张弓 <sz9214e@qq.com>
2021-03-21 20:13:04 +0800

    从AbstractField中分离出相应的属性

--- summer-ui/src/main/java/cn/cerc/ui/fields/TextAreaField.java
+++ summer-ui/src/main/java/cn/cerc/ui/fields/TextAreaField.java
@@ -3,23 +3,99 @@ package cn.cerc.ui.fields;
 import cn.cerc.core.Record;
 import cn.cerc.ui.parts.UIComponent;
 
-public class TextAreaField extends AbstractField {
+// TODO 专用于textarea标签，需要拆分该标签出来，黄荣君 2016-05-31
+public class TextAreaField extends AbstractField implements IFieldPlaceholder, IFieldRequired, IFieldTextArea {
+    private String placeholder;
+    private boolean required;
+    // 最大字符串数
+    private int maxlength;
+    // 可见行数
+    private int rows;
+    // 可见宽度
+    private int cols;
+    // 是否禁用
+    private boolean resize = true;
+
     public TextAreaField(UIComponent owner, String name, String field) {
         super(owner, name, 0);
         this.setField(field);
-        this.setHtmlTag("textarea");
     }
 
     public TextAreaField(UIComponent owner, String name, String field, int width) {
         super(owner, name, 0);
         this.setField(field);
         this.setWidth(width);
-        this.setHtmlTag("textarea");
     }
 
     @Override
     public String getText(Record record) {
         return getDefaultText(record);
+    }
+
+    @Override
+    public String getPlaceholder() {
+        return placeholder;
+    }
+
+    @Override
+    public TextAreaField setPlaceholder(String placeholder) {
+        this.placeholder = placeholder;
+        return this;
+    }
+
+    @Override
+    public boolean isRequired() {
+        return required;
+    }
+
+    @Override
+    public TextAreaField setRequired(boolean required) {
+        this.required = required;
+        return this;
+    }
+
+    @Override
+    public int getMaxlength() {
+        return maxlength;
+    }
+
+    @Override
+    public TextAreaField setMaxlength(int maxlength) {
+        this.maxlength = maxlength;
+        return this;
+    }
+
+    @Override
+    public int getRows() {
+        return rows;
+    }
+
+    @Override
+    public TextAreaField setRows(int rows) {
+        this.rows = rows;
+        return this;
+    }
+
+    @Override
+    public int getCols() {
+        return cols;
+    }
+
+    @Override
+    public TextAreaField setCols(int cols) {
+        this.cols = cols;
+        return this;
+    }
+
+    @Override
+    public boolean isResize() {
+        return resize;
+    }
+
+    @Override
+    public TextAreaField setResize(boolean resize) {
+        this.resize = resize;
+        return this;
     }
 
 }
--- summer-ui/src/main/java/cn/cerc/ui/fields/UploadField.java
+++ summer-ui/src/main/java/cn/cerc/ui/fields/UploadField.java
@@ -3,7 +3,9 @@ package cn.cerc.ui.fields;
 import cn.cerc.core.Record;
 import cn.cerc.ui.parts.UIComponent;
 
-public class UploadField extends AbstractField {
+public class UploadField extends AbstractField implements IFieldMultiple {
+    // 用于文件上传是否可以选则多个文件
+    private boolean multiple = false;
 
     public UploadField(UIComponent owner, String name, String field) {
         super(owner, name, 5);
@@ -14,5 +16,16 @@ public class UploadField extends AbstractField {
     @Override
     public String getText(Record record) {
         return record.getString(field);
+    }
+
+    @Override
+    public boolean isMultiple() {
+        return multiple;
+    }
+
+    @Override
+    public UploadField setMultiple(boolean multiple) {
+        this.multiple = multiple;
+        return this;
     }
 }
