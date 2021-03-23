0793ccac25eecfdb682cb95fb49dac4caab3eeef
张弓 <sz9214e@qq.com>
2021-03-23 22:16:29 +0800

    Update DateTimeField.java

--- summer-ui/src/main/java/cn/cerc/ui/fields/DateTimeField.java
+++ summer-ui/src/main/java/cn/cerc/ui/fields/DateTimeField.java
@@ -98,34 +98,7 @@ public class DateTimeField extends AbstractField implements IColumn, IFieldBuild
 
     @Override
     public void outputColumn(HtmlWriter html) {
-        Record record = getRecord();
-
-        IFieldBuildUrl obj = null;
-        if (this instanceof IFieldBuildUrl) {
-            obj = (IFieldBuildUrl) this;
-        }
-
-        if (obj != null && obj.getBuildUrl() != null) {
-            UrlRecord url = new UrlRecord();
-            obj.getBuildUrl().buildUrl(record, url);
-            if (!"".equals(url.getUrl())) {
-                html.print("<a href=\"%s\"", url.getUrl());
-                if (url.getTitle() != null) {
-                    html.print(" title=\"%s\"", url.getTitle());
-                }
-                if (url.getTarget() != null) {
-                    html.print(" target=\"%s\"", url.getTarget());
-                }
-                if (url.getHintMsg() != null) {
-                    html.print(" onClick=\"return confirm('%s');\"", url.getHintMsg());
-                }
-                html.print(">%s</a>", this.getText());
-            } else {
-                html.print(this.getText());
-            }
-        } else {
-            html.print(this.getText());
-        }
+        html.print(this.getText());
     }
 
 }
