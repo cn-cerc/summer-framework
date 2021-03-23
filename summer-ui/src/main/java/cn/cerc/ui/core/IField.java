9b96df0e2cbfc56bf6c8cc58e816ad8ae495cdc6
张弓 <sz9214e@qq.com>
2021-03-23 20:19:51 +0800

    Update IField.java

--- summer-ui/src/main/java/cn/cerc/ui/core/IField.java
+++ summer-ui/src/main/java/cn/cerc/ui/core/IField.java
@@ -3,13 +3,12 @@ package cn.cerc.ui.core;
 import cn.cerc.core.Record;
 
 public interface IField extends IReadonlyOwner, INameOwner {
+    
     String getTitle();
 
     String getField();
 
-    default int getWidth() {
-        return 1;
-    }
+    int getWidth();
 
     String getAlign();
 
