e44dc782ab1482ae33557c17bdda4e69a0daa1ca
张弓 <sz9214e@qq.com>
2021-03-24 19:14:22 +0800

    Create UIMemo.java

---
+++ summer-ui/src/main/java/cn/cerc/ui/vcl/UIMemo.java
@@ -0,0 +1,15 @@
+package cn.cerc.ui.vcl;
+
+import cn.cerc.ui.parts.UIComponent;
+
+public class UIMemo extends UITextArea {
+
+    public UIMemo() {
+        super();
+    }
+    
+    public UIMemo(UIComponent owner) {
+        super(owner);
+    }
+
+}
