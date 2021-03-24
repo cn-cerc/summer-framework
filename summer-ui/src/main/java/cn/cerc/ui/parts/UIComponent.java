d328515218b6f2db2a9dae84c67a4898e8d73274
张弓 <sz9214e@qq.com>
2021-03-24 10:14:40 +0800

    增加Iterable接口

--- summer-ui/src/main/java/cn/cerc/ui/parts/UIComponent.java
+++ summer-ui/src/main/java/cn/cerc/ui/parts/UIComponent.java
@@ -1,9 +1,13 @@
 package cn.cerc.ui.parts;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import cn.cerc.ui.core.Component;
 import cn.cerc.ui.core.HtmlWriter;
 
-public abstract class UIComponent extends Component {
+public abstract class UIComponent extends Component implements Iterable<UIComponent> {
 
     public UIComponent() {
         super();
@@ -20,6 +24,17 @@ public abstract class UIComponent extends Component {
         HtmlWriter html = new HtmlWriter();
         output(html);
         return html.toString();
+    }
+
+    @Override
+    public Iterator<UIComponent> iterator() {
+        List<UIComponent> list = new ArrayList<>();
+        for(Component component: this.getComponents()) {
+            if(component instanceof UIComponent) {
+                list.add((UIComponent) component);
+            }
+        }
+        return list.iterator();
     }
 
 }
