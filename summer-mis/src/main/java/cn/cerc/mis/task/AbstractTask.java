990cf8ac5d52e8b35636312ff0a9d5466fdef11d
李远 <460289628@qq.com>
2021-03-24 15:08:17 +0800

    修复队列空指针异常问题

--- summer-mis/src/main/java/cn/cerc/mis/task/AbstractTask.java
+++ summer-mis/src/main/java/cn/cerc/mis/task/AbstractTask.java
@@ -2,10 +2,10 @@ package cn.cerc.mis.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import cn.cerc.core.ISession;
 import cn.cerc.mis.core.Application;
 import cn.cerc.mis.core.Handle;
 import cn.cerc.mis.core.ISystemTable;
+import cn.cerc.mis.custom.SessionDefault;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,8 +14,6 @@ public abstract class AbstractTask extends Handle implements Runnable {
     @Autowired
     public ISystemTable systemTable;
     private String describe;
-
-    private ISession session;
 
     /**
      * 缓存过期时间 单位：秒
@@ -52,25 +50,16 @@ public abstract class AbstractTask extends Handle implements Runnable {
      */
     @Override
     public void run() {
-        systemTable = Application.getSystemTable();
+        SessionDefault session = new SessionDefault();
         try {
             this.setHandle(new Handle(session));
+            session.setProperty(Application.userCode, "admin");
             this.execute();
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         } finally {
             session.close();
         }
-    }
-
-    @Override
-    public ISession getSession() {
-        return session;
-    }
-
-    @Override
-    public void setSession(ISession session) {
-        this.session = session;
     }
 
     // 具体业务逻辑代码
