2673358c5bfbf3ece1629558c9bb6ad9c67144d3
张弓 <sz9214e@qq.com>
2021-03-26 06:34:34 +0800

    Update IBufferKey.java

--- summer-mis/src/main/java/cn/cerc/mis/core/IBufferKey.java
+++ summer-mis/src/main/java/cn/cerc/mis/core/IBufferKey.java
@@ -2,7 +2,7 @@ package cn.cerc.mis.core;
 
 public interface IBufferKey {
 
-    // 起点值，框架级从0开始，应用级应从100开始
+    // 起点值，框架级从0开始，应用级应从1000开始
     int getStartingPoint();
 
     int getMinimumNumber();
