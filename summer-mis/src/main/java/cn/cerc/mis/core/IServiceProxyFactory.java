2dad7217f4e9e93950255d35b7bc51e5590ad029
张弓 <sz9214e@qq.com>
2021-03-27 16:13:35 +0800

    Update IServiceProxyFactory.java

--- summer-mis/src/main/java/cn/cerc/mis/core/IServiceProxyFactory.java
+++ summer-mis/src/main/java/cn/cerc/mis/core/IServiceProxyFactory.java
@@ -4,13 +4,16 @@ import cn.cerc.db.core.IHandle;
 import cn.cerc.db.core.ISessionOwner;
 import cn.cerc.mis.client.IServiceProxy;
 
-public interface IServiceProxyFactory extends ISessionOwner{
-    
+@Deprecated
+public interface IServiceProxyFactory extends ISessionOwner {
+
     /**
      * 创建对象，返回 LocalService 或 RemoteService
+     * 
      * @param handle
      * @param corpNo
      * @return
      */
+    @Deprecated
     IServiceProxy get(IHandle handle, String corpNo);
 }
