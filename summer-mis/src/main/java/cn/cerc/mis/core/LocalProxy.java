b92820a18b3897ccf4e700f96d82c92d123175b8
张弓 <sz9214e@qq.com>
2021-03-28 12:04:56 +0800

    Update LocalProxy.java

--- summer-mis/src/main/java/cn/cerc/mis/core/LocalProxy.java
+++ summer-mis/src/main/java/cn/cerc/mis/core/LocalProxy.java
@@ -10,6 +10,30 @@ public class LocalProxy extends CustomLocalProxy {
         super(handle);
     }
 
+    public DataSet exec(String... args) {
+        Object bean = this.getServiceObject();
+        if (bean == null)
+            return null;
+
+        DataSet dataIn = new DataSet();
+        if (args.length > 0) {
+            Record headIn = dataIn.getHead();
+            if (args.length % 2 != 0) {
+                // TODO 此处应该使用 ClassResource
+                throw new RuntimeException("传入的参数数量必须为偶数！");
+            }
+            for (int i = 0; i < args.length; i = i + 2) {
+                headIn.setField(args[i].toString(), args[i + 1]);
+            }
+        }
+
+        DataSet dataOut = new DataSet();
+        if (executeService(bean, dataIn, dataOut))
+            return dataOut;
+        else
+            return null;
+    }
+
     public DataSet exec(Record headIn) {
         Object bean = this.getServiceObject();
         if (bean == null)
