7cc437c5b52e525d70d80288a6c25020e692379b
张弓 <sz9214e@qq.com>
2021-03-28 13:09:28 +0800

    调整函数位置，增加兼容性 (#221)

--- summer-mis/src/main/java/cn/cerc/mis/core/IDataService.java
+++ summer-mis/src/main/java/cn/cerc/mis/core/IDataService.java
@@ -9,4 +9,28 @@ public interface IDataService extends IHandle {
     // 数据库连接
     void setHandle(IHandle handle);
 
+    default ServiceStatus fail(String format, Object... args) {
+        ServiceStatus status = new ServiceStatus(false);
+        if (args.length > 0) {
+            status.setMessage(String.format(format, args));
+        } else {
+            status.setMessage(format);
+        }
+        return status;
+    }
+
+    default ServiceStatus success() {
+        return new ServiceStatus(true);
+    }
+
+    default ServiceStatus success(String format, Object... args) {
+        ServiceStatus status = new ServiceStatus(true);
+        if (args.length > 0) {
+            status.setMessage(String.format(format, args));
+        } else {
+            status.setMessage(format);
+        }
+        return status;
+    }
+
 }
