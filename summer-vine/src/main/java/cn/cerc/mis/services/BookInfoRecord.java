e50bae930477185f131408feed800523903adb6f
IT君 <l1091462907@gmail.com>
2021-03-31 21:25:22 +0800

    Update BookInfoRecord.java

--- summer-vine/src/main/java/cn/cerc/mis/services/BookInfoRecord.java
+++ summer-vine/src/main/java/cn/cerc/mis/services/BookInfoRecord.java
@@ -1,10 +1,5 @@
 package cn.cerc.mis.services;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class BookInfoRecord {
     private int status;
     private String code;
@@ -22,4 +17,133 @@ public class BookInfoRecord {
     private String currency;
     private String email;
     private String fax;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCorpType() {
+        return corpType;
+    }
+
+    public void setCorpType(int corpType) {
+        this.corpType = corpType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getFastTel() {
+        return fastTel;
+    }
+
+    public void setFastTel(String fastTel) {
+        this.fastTel = fastTel;
+    }
+
+    public String getManagerPhone() {
+        return managerPhone;
+    }
+
+    public void setManagerPhone(String managerPhone) {
+        this.managerPhone = managerPhone;
+    }
+
+    public String getStartHost() {
+        return startHost;
+    }
+
+    public void setStartHost(String startHost) {
+        this.startHost = startHost;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public boolean isAuthentication() {
+        return authentication;
+    }
+
+    public void setAuthentication(boolean authentication) {
+        this.authentication = authentication;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
 }
