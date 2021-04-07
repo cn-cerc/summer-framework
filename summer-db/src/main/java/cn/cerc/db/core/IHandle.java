b6a64c7cd478cb9f6692dbef809e80f3c8003c3a
张弓 <sz9214e@qq.com>
2021-04-07 21:52:59 +0800

    Update IHandle.java

--- summer-db/src/main/java/cn/cerc/db/core/IHandle.java
+++ summer-db/src/main/java/cn/cerc/db/core/IHandle.java
@@ -1,19 +1,5 @@
 package cn.cerc.db.core;
 
-import cn.cerc.db.mysql.MysqlConnection;
-
 public interface IHandle extends ISessionOwner {
-
-    default String getCorpNo() {
-        return getSession().getCorpNo();
-    }
-
-    default String getUserCode() {
-        return getSession().getUserCode();
-    }
-
-    default MysqlConnection getConnection() {
-        return (MysqlConnection) getSession().getProperty(MysqlConnection.sessionId);
-    }
 
 }
