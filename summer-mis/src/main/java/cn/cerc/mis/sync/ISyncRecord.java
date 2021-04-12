7e9d1d1af0e7aa786ee7ded7de5852a7ce46da29
张弓 <sz9214e@qq.com>
2021-04-12 14:41:37 +0800

    完善同步工具

---
+++ summer-mis/src/main/java/cn/cerc/mis/sync/ISyncRecord.java
@@ -0,0 +1,13 @@
+package cn.cerc.mis.sync;
+
+import cn.cerc.core.Record;
+
+public interface ISyncRecord {
+    
+    boolean onAppend(Record newRecord);
+
+    boolean onDelete(Record current);
+    
+    boolean onUpdate(Record current, Record newRecord);
+
+}
---
+++ summer-mis/src/main/java/cn/cerc/mis/sync/SyncOpera.java
@@ -0,0 +1,6 @@
+package cn.cerc.mis.sync;
+
+public enum SyncOpera {
+    
+    Append, Delete, Update;
+}
