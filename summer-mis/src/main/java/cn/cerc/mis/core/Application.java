badfbf252b33ae1928eb6d5cecc62bc59f8d38a3
张弓 <sz9214e@qq.com>
2021-04-14 19:57:57 +0800

    为form等增加plugins支持

--- summer-mis/src/main/java/cn/cerc/mis/core/Application.java
+++ summer-mis/src/main/java/cn/cerc/mis/core/Application.java
@@ -393,6 +393,64 @@ public class Application {
     }
 
     /**
+     * 判断当前公司别当前对象，是否存在插件，如FrmProduct_131001（必须继承IPlugins）
+     * 
+     * @param owner
+     * @return 返回true or false
+     */
+    public static boolean existsPlugins(Object owner) {
+        if (context == null)
+            return false;
+        String names[] = owner.getClass().getName().split("\\.");
+        String corpNo = null;
+        if (owner instanceof ISessionOwner)
+            corpNo = ((ISessionOwner) owner).getCorpNo();
+        if (corpNo == null || "".equals(corpNo))
+            return false;
+        String target = names[names.length - 1] + "_" + corpNo;
+        target = target.substring(0, 1).toLowerCase() + target.substring(1, target.length());
+        String[] beans = context.getBeanNamesForType(IPlugins.class);
+        for (String item : beans) {
+            if (item.equals(target))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * 返回当前公司别当前对象之之插件对象，如FrmProduct_131001（必须继承IPlugins）
+     * 
+     * @param owner
+     * @return 返回插件对象，或返回null
+     */
+    public static <T> T getPlugins(Object owner, Class<T> requiredType) {
+        if (context == null)
+            return null;
+        String names[] = owner.getClass().getName().split("\\.");
+        String corpNo = null;
+        if (owner instanceof ISessionOwner)
+            corpNo = ((ISessionOwner) owner).getCorpNo();
+        if (corpNo == null || "".equals(corpNo))
+            return null;
+        String target = names[names.length - 1] + "_" + corpNo;
+        target = target.substring(0, 1).toLowerCase() + target.substring(1, target.length());
+        T result = context.getBean(target, requiredType);
+        if (result != null) {
+            // 要求必须继承IPlugins
+            if (result instanceof IPlugins) {
+                ((IPlugins) result).setOwner(owner);
+            } else {
+                log.warn("{} not supports IPlugins.", target);
+                return null;
+            }
+            if (result instanceof ISessionOwner) {
+                ((ISessionOwner) result).setSession(((ISessionOwner) owner).getSession());
+            }
+        }
+        return result;
+    }
+
+    /**
      * 获得 token 的值
      * 
      * @param handle
---
+++ summer-mis/src/main/java/cn/cerc/mis/core/IPlugins.java
@@ -0,0 +1,10 @@
+package cn.cerc.mis.core;
+
+/**
+ * 为系统提供客制化支持
+ *
+ */
+public interface IPlugins {
+
+    void setOwner(Object owner);
+}
