27b9fc2bff1967e03ba69d3cee23a7afb27353ad
黄荣君 <l1091462907@gmail.com>
2021-04-19 20:12:06 +0800

    Update SyncServerRedis.java

--- summer-mis/src/main/java/cn/cerc/mis/sync/SyncServerRedis.java
+++ summer-mis/src/main/java/cn/cerc/mis/sync/SyncServerRedis.java
@@ -1,5 +1,9 @@
 package cn.cerc.mis.sync;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cn.cerc.core.ClassConfig;
 import cn.cerc.core.ISession;
 import cn.cerc.core.Record;
 import cn.cerc.db.redis.JedisFactory;
@@ -9,6 +13,8 @@ import cn.cerc.mis.other.MemoryBuffer;
 import redis.clients.jedis.Jedis;
 
 public class SyncServerRedis implements ISyncServer {
+    private static final Logger log = LoggerFactory.getLogger(SyncServerRedis.class);
+
     private SyncServer pushQueue;
     private SyncServer popQueue;
 
@@ -26,10 +32,10 @@ public class SyncServerRedis implements ISyncServer {
 
     @Override
     public void repush(ISession session, Record record) {
-        if (pushQueue == null)
-            throw new RuntimeException("pushQueue is null");
-        String buffKey = MemoryBuffer.buildKey(pushQueue);
-        String configKey = "sync." + pushQueue.name().toLowerCase();
+        if (popQueue == null)
+            throw new RuntimeException("popQueue is null");
+        String buffKey = MemoryBuffer.buildKey(popQueue);
+        String configKey = "sync." + popQueue.name().toLowerCase();
         try (Jedis jedis = JedisFactory.getJedis(configKey)) {
             // 从右边推入
             jedis.rpush(buffKey, record.toString());
@@ -52,7 +58,12 @@ public class SyncServerRedis implements ISyncServer {
 
                 Record record = new Record();
                 record.setJSON(data);
-                popProcesser.popRecord(session, record);
+                try {
+                    popProcesser.popRecord(session, record);
+                } catch (Exception e) {
+                    log.error(record.toString());
+                    e.printStackTrace();
+                }
             }
         }
         return maxRecords;
