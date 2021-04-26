4f182385f5c92babad89a282004562f4b44f7999
张弓 <sz9214e@qq.com>
2021-04-26 11:52:06 +0800

    防止历史版本找不到对应关系

---
+++ summer-mis/src/main/java/cn/cerc/mis/core/ClientDevice.java
@@ -0,0 +1,8 @@
+package cn.cerc.mis.core;
+
+@Deprecated
+public class ClientDevice extends AppClient {
+    private static final long serialVersionUID = 3637149857407787553L;
+    public static String device_pc = pc;
+
+}
---
+++ summer-ui/src/main/java/cn/cerc/ui/vcl/UIActionForm.java
@@ -0,0 +1,12 @@
+package cn.cerc.ui.vcl;
+
+import cn.cerc.ui.parts.UIComponent;
+
+@Deprecated
+public class UIActionForm extends UIForm {
+
+    public UIActionForm(UIComponent owner) {
+       super(owner);
+    }
+
+}
