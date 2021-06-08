977c2f0196f3a4276e4fcecec2a35677cb82c88c
黄荣君 <l1091462907@gmail.com>
2021-06-08 10:52:24 +0800

    !37 合并HistoryLevel和HistoryRecord，简化用户调用

--- summer-mis/src/main/java/cn/cerc/mis/tools/ExportHistoryWriter.java
+++ summer-mis/src/main/java/cn/cerc/mis/tools/ExportHistoryWriter.java
@@ -6,7 +6,7 @@ import cn.cerc.mis.SummerMIS;
 import cn.cerc.mis.excel.output.ExcelTemplate;
 import cn.cerc.mis.excel.output.HistoryWriter;
 import cn.cerc.mis.other.HistoryLevel;
-import cn.cerc.mis.other.HistoryRecord;
+
 
 public class ExportHistoryWriter implements HistoryWriter {
     private static final ClassResource res = new ClassResource(ExportHistoryWriter.class, SummerMIS.ID);
@@ -19,6 +19,6 @@ public class ExportHistoryWriter implements HistoryWriter {
     public void finish(Object handle, ExcelTemplate template) {
         IHandle appHandle = (IHandle) handle;
         String log = String.format(res.getString(1, "系统已经为您导出: %s.xls"), template.getFileName());
-        new HistoryRecord(log).setLevel(HistoryLevel.General).save(appHandle);
+        HistoryLevel.General.append(appHandle, log);
     }
 }
