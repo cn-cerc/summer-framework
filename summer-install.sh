2d3c7f6a6d35e050c7d0b139eb51c8f86e34ba5a
IT君 <l1091462907@gmail.com>
2021-07-19 20:46:49 +0800

    增加自动构建工具

---
+++ summer-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mvn clean install
+
+cd ..
+cd summer-model
+mvn clean install
+
+cd ..
+cd summer-view
+mvn clean install
+
+cd ..
+cd summer-controller
+mvn clean install
+
+cd ..
+cd summer-local
+mvn clean install
