a5beeae1c567ae742d4e7c9f0929b8ab53e74311
IT君 <l1091462907@gmail.com>
2021-07-21 11:15:35 +0800

    Create summer-clone.sh

---
+++ summer-clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone git@gitee.com:mimrc/summer-framework.git
+
+git clone git@gitee.com:mimrc/summer-model.git
+
+git clone git@gitee.com:mimrc/summer-view.git
+
+git clone git@gitee.com:mimrc/summer-controller.git
+
+git clone git@gitee.com:mimrc/summer-local.git
+
