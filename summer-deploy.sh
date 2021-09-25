f128e3b8067cce323345644d4743a9ea35a5eef0
IT君 <l1091462907@gmail.com>
2021-09-25 10:24:33 +0800

    调整发布脚本

--- summer-deploy.sh
+++ summer-deploy.sh
@@ -4,16 +4,20 @@ mvn clean deploy
 
 cd ..
 cd summer-model
+git pull
 mvn clean deploy
 
 cd ..
 cd summer-controller
+git pull
 mvn clean deploy
 
 cd ..
 cd summer-view
+git pull
 mvn clean deploy
 
 cd ..
 cd summer-local
+git pull
 mvn clean deploy
