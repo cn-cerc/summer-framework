f209200e2693338f143c1cb436d67a1a5fc055c5
HUANG Rongjun <l1091462907@gmail.com>
2022-11-08 11:11:25 +0800

    调整自动切换分支的脚本

--- switch-to-branch.sh
+++ switch-to-branch.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-git checkout develop
+branch=$1
+
+git checkout $branch
 git pull
 
 cd ..
 cd summer-data
-git checkout develop
+git checkout $branch
 git pull
 
 cd ..
 cd summer-mis
-git checkout develop
+git checkout $branch
 git pull
 
 cd ..
 cd summer-view
-git checkout develop
+git checkout $branch
 git pull
 
 cd ..
 cd summer-local
-git checkout develop
+git checkout $branch
 git pull
