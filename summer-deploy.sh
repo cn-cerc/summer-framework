35a05b49e24a48de7ec833f82aa230fb542763b3
黄荣君 <l1091462907@gmail.com>
2023-05-30 04:03:12 +0000

    !18 调整框架的打包和发布脚本

--- summer-deploy.sh
+++ summer-deploy.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
+pwd
 git pull
-mvn clean deploy
+mvn -T 1C clean deploy
 
 cd ..
 cd summer-data
 git pull
-mvn clean deploy
+mvn -T 1C clean deploy
 
 cd ..
 cd summer-mis
 git pull
-mvn clean deploy
+mvn -T 1C  clean deploy
 
 cd ..
 cd summer-view
 git pull
-mvn clean deploy
+mvn -T 1C clean deploy
 
 cd ..
 cd summer-local
 git pull
-mvn clean deploy
+mvn -T 1C clean deploy
