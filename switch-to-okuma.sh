9f7922372bb2550ee95949131222285bed9a4a5f
李远 <460289628@qq.com>
2024-06-25 20:51:59 +0800

    Update switch-to-okuma.sh

--- switch-to-okuma.sh
+++ switch-to-okuma.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-git checkout beta
+git checkout okuma
 git pull
 
 cd ..
 cd summer-model
-git checkout beta
+git checkout okuma
 git pull
 
 cd ..
 cd summer-controller
-git checkout beta
+git checkout okuma
 git pull
 
 cd ..
 cd summer-view
-git checkout beta
+git checkout okuma
 git pull
 
 cd ..
 cd summer-local
-git checkout beta
+git checkout okuma
 git pull
